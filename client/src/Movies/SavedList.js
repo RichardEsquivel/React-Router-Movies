a6761569594ef25e110a1dc721b440efb9a7cdd3
RichardEsquivel <rlesquivel@gmail.com>
2019-08-20 17:58:42 -0500

    Completed MVP

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,16 @@
 import React from 'react';
+//Importing Link from React-router-dom in order to create a link
+//that will take the HomeButton Icon navigate back to the home page
+import { Link } from 'react-router-dom';
 
 const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
+	<div className="saved-list">
+		<h3>Saved Movies:</h3>
+		{props.list.map(movie => (
+			<span className="saved-movie">{movie.title}</span>
+		))}
+		<Link exact to="/" className="home-button">Home</Link>
+	</div>
 );
 
 export default SavedList;
